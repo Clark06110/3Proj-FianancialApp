4e3c901b479511ff869514b93dc13c6a4fe996ef
Yoann Chhang <yoann.chhang@gmail.com>
2024-01-09 14:29:18 +0100

    Add Router and Test page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,14 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 // Import your screens/components here
 import HomeScreen from '~/pages/Home';
+import TestScreen from '~/pages/Test';
 
 const Stack = createStackNavigator();
 
 const Router: React.FC = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName='Home'>
                 <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Test" component={TestScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
